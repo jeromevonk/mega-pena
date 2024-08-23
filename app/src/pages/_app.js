4af76e084a6aef12d102364ab6425ed084d958bb
Jerome Vonk <lelovv@gmail.com>
2024-08-23 17:58:53 -0300

    propTypes for _app

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,5 +1,11 @@
 import '../style/index.css'
+import PropTypes from 'prop-types';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
